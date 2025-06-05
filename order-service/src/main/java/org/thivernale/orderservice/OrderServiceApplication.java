20e2d91417ffadd79cbf8272ce3130ae60dd89b3
thivernale <thivernale@gmail.com>
2025-06-05 14:47:02 +0300

    Order Service: add scheduler synchronization over multiple instances

--- order-service/src/main/java/org/thivernale/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/org/thivernale/orderservice/OrderServiceApplication.java
@@ -1,5 +1,6 @@
 package org.thivernale.orderservice;
 
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "PT10M")
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
--- order-service/src/main/java/org/thivernale/orderservice/jobs/OrderEventPublisherJob.java
+++ order-service/src/main/java/org/thivernale/orderservice/jobs/OrderEventPublisherJob.java
@@ -1,6 +1,8 @@
 package org.thivernale.orderservice.jobs;
 
 import lombok.RequiredArgsConstructor;
+import net.javacrumbs.shedlock.core.LockAssert;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.thivernale.orderservice.domain.OrderEventService;
@@ -11,7 +13,9 @@ class OrderEventPublisherJob {
     private final OrderEventService orderEventService;
 
     @Scheduled(cron = "${app.schedule.publish-events:-}")
+    @SchedulerLock(name = "publishOrderEvents")
     public void publishEvents() {
+        LockAssert.assertLocked();
         orderEventService.publishEvents();
     }
 }
