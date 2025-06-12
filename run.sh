1e0c40c42300d837d5ad7db1257bf06dcbc8dad5
thivernale <thivernale@gmail.com>
2025-06-12 14:45:45 +0300

    Add observability and centralized logging

--- run.sh
+++ run.sh
@@ -4,10 +4,12 @@ ACTION=${1:-up}
 
 if [ "$ACTION" = "down" ]; then
   echo 'down'
-  docker compose -f deployment/docker-compose/infra.yml -f deployment/docker-compose/apps.yml down catalog-service order-service notification-service api-gateway
+  docker compose -f deployment/docker-compose/infra.yml -f deployment/docker-compose/apps.yml down \
+  notification-service api-gateway \
+  catalog-service order-service
 else
 #./mvnw -pl catalog-service/ spring-boot:build-image -DskipTests
   echo 'up'
-  docker compose -f deployment/docker-compose/infra.yml -f deployment/docker-compose/apps.yml up -d
+  docker compose -f deployment/docker-compose/infra.yml -f deployment/docker-compose/apps.yml -f deployment/docker-compose/monitoring.yml up -d
 fi
 exit 0
