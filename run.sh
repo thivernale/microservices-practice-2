a76ff0cf756440b977ae1ec0373a9adb756e9874
thivernale <thivernale@gmail.com>
2025-06-13 16:04:18 +0300

    Add distributed tracing

--- run.sh
+++ run.sh
@@ -4,12 +4,11 @@ ACTION=${1:-up}
 
 if [ "$ACTION" = "down" ]; then
   echo 'down'
-  docker compose -f deployment/docker-compose/infra.yml -f deployment/docker-compose/apps.yml down \
-  notification-service api-gateway \
-  catalog-service order-service
+  docker compose -f deployment/docker-compose/infra.yml -f deployment/docker-compose/apps.yml -f deployment/docker-compose/monitoring.yml down \
+  #notification-service api-gateway catalog-service order-service
 else
-#./mvnw -pl catalog-service/ spring-boot:build-image -DskipTests
   echo 'up'
+  #./mvnw -pl catalog-service/ spring-boot:build-image -DskipTests
   docker compose -f deployment/docker-compose/infra.yml -f deployment/docker-compose/apps.yml -f deployment/docker-compose/monitoring.yml up -d
 fi
 exit 0
