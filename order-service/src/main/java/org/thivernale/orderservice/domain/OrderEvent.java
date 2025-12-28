9f8c20014bff0baaf8e1c4e00e1806deaa823126
thivernale <thivernale@gmail.com>
2025-12-28 14:50:16 +0200

    Pessimistic locking and ORM conversion practice

--- order-service/src/main/java/org/thivernale/orderservice/domain/OrderEvent.java
+++ order-service/src/main/java/org/thivernale/orderservice/domain/OrderEvent.java
@@ -25,14 +25,31 @@ class OrderEvent {
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
+    //@Convert(converter = OrderEventTypeConverter.class)
     private OrderEventType type;
 
     @Column(nullable = false)
     private String payload;
+
+    /*@Column(nullable = false, columnDefinition = "jsonb")
+    @ColumnTransformer(write = "?::jsonb")
+    private String payloadJsonb;*/
 
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt = LocalDateTime.now();
 
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    private static class OrderEventTypeConverter implements AttributeConverter<OrderEventType, String> {
+        @Override
+        public String convertToDatabaseColumn(OrderEventType attribute) {
+            return attribute.name();
+        }
+
+        @Override
+        public OrderEventType convertToEntityAttribute(String dbData) {
+            return OrderEventType.valueOf(dbData);
+        }
+    }
 }
--- order-service/src/main/java/org/thivernale/orderservice/domain/OrderEventRepository.java
+++ order-service/src/main/java/org/thivernale/orderservice/domain/OrderEventRepository.java
@@ -1,6 +1,16 @@
 package org.thivernale.orderservice.domain;
 
+import jakarta.persistence.LockModeType;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.thivernale.orderservice.domain.models.OrderEventType;
 
-public interface OrderEventRepository extends JpaRepository<OrderEvent, Long> {
+import java.util.List;
+
+interface OrderEventRepository extends JpaRepository<OrderEvent, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT o FROM OrderEvent o WHERE o.type = :type")
+    List<OrderEvent> findOrderEventsForProcessing(OrderEventType type, Pageable pageable);
 }
